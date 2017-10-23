bb7b8d645e5c964ea3c24f93f460c4221c2ded72
minivesper <minivesper@live.com>
2017-10-23 11:10:28 -0600

    requests

--- currentBuild/Requests.py
+++ currentBuild/Requests.py
@@ -3,21 +3,60 @@ import getpass
 # import inquirer
 from pprint import pprint
 
-class Requests:
-    def __init__():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket = s
-        s.connect((self.TCP_IP, self.TCP_PORT))
+# class Requests:
+#     def __init__():
+#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         self.socket = s
+#         s.connect((self.TCP_IP, self.TCP_PORT))
+
+class LOGN:
+    def __init__(self,Username,passwd):
+        self.Username = Username
+        self.passwd = passwd
+
+    def getUsername(self):
+        return self.Username
+
+    def getPass(self):
+        return self.passwd
+
+    def encode(self):
+        sendStr = "LOGN"
+        sendStr += str(len(self.getUsername())) + "|" + self.getUsername() + "|"+ str(len(self.getPass())) + "|" + self.getPass()
+        return sendStr
+
+    def decode(self, stream):
+        s = stream.split("|")
+        self.Username = s[1]
+        self.passwd = s[3]
+
+    def __repr__(self):
+        return("LOGN: %s %s"%(self.getUsername(), self.getPass()))
+    
+    # def decode(self, stream):
+    #     userlen = ""
+    #     user = ""
+    #     passlen = ""
+    #     password = ""
+    #     for i in range(4, len(stream)):
+    #         if(stream[i] != "|" and isinstance(userlen, str)):
+    #             userlen += stream[i]
+    #         elif(stream[i] == "|" and passlen == ""):
+    #             userlen = int(userlen)
+    #             print(userlen)
+    #         elif(stream[i] != "|" and isinstance(userlen, int)):
+    #             passlen += stream[i]
+    #         elif(stream[i] == "|" and isinstance(userlen, int)):
+    #             passlen = int(passlen)
+    #     print(passlen)
+    #     print(userlen)
+
 
 class SMSG:
     def __init__(self, Username, Recipient, Message):
         self.Username = Username
         self.Recipient = Recipient
         self.Message = Message
-
-    def encode(self):
-        sendStr = "SMSG"
-
 
     def getUsername(self):
         return self.Username
